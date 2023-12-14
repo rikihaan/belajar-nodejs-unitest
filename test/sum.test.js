bd4b6b6b5bd954b547e9f0413861f2e8c15bba19
Asep <asepriki2014@gmail.com>
2023-12-14 14:25:18 +0700

    belajar verbose

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,6 +1,16 @@
 import {sum} from '../src/sum.js';
 
-test("sum(1,2) must be 3",()=>{
+test("test sum 1 , sum(1,2) must be 3",()=>{
+    const result = sum(1,2);
+    expect(result).toBe(3);
+})
+
+test("test sum 2, sum(1,2) must be 3",()=>{
+    const result = sum(1,2);
+    expect(result).toBe(3);
+})
+
+test("test sum 3, sum(1,2) must be 3",()=>{
     const result = sum(1,2);
     expect(result).toBe(3);
 })
